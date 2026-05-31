182cc6facd2e58353ed56cca523bc5c84079f3bb
Scorpion-Qiang <qiangzaibuqiang@163.com>
2026-05-31 19:14:35 +0800

    numpy

--- neuq/l_numpy.py
+++ neuq/l_numpy.py
@@ -11,7 +11,7 @@ print(b)
 
 print(a.ndim)  # 维度
 
-print(b.shape) # （行, 列）
+print(b.shape) # （层, 行, 列）
 
 print(a.dtype) # 数据类型   int64：一个数据占 64 个 bit 位（8个字节）
 print(c.dtype)
@@ -40,12 +40,12 @@ print(d[0, 1: 6: 2])
 # 修改元素
 print('修改元素')
 print(d)
-# d[2, 3] = -20
+d[2, 3] = -20
 
-# d[0, :] = 1    # 修改整行
-# d[0, :] = [8, 7, 6, 5, 4, 3, 2, 1]
+d[0, :] = 1    # 修改整行
+d[0, :] = [8, 7, 6, 5, 4, 3, 2, 1]
 
-# d[:, 1] = [1, 1, 0]   # 修改整列
+d[:, 1] = [1, 1, 0]   # 修改整列
 
 d[1:3, 1:4] = 1        # [1, 3) 行 [1, 4) 列, 修改
 d[1:3, 1:4] = [[1, 2, 3], [4, 5, 6]]
@@ -71,7 +71,7 @@ b = np.zeros((2, 3, 3), 'int32')
 print(a)
 print(b)
 
-a = np.ones((2, 3), dtype= 'int32')
+a = np.ones((2, 3), 'int32')
 print(a)
 
 a = np.full((3, 3, 3), 99)
@@ -132,3 +132,80 @@ zeros[1, 1] = 9
 output[1:-1, 1:-1] = zeros
 
 print(output)
+
+# 复制
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+b = a.copy()
+b[0] = 0
+
+print(a)
+print(b)
+
+# 数学运算
+print(a)
+print(b)
+print(a + 2)
+print(a * 2)
+print(a ** 2)
+print(a + b)
+print(a * b)   # 对应元素相乘, 要求 a、b 的形状相同
+
+print(np.sin(a))
+
+# 线性代数
+a = np.full((2, 3), 2)
+b = np.ones((3, 4), 'int32')
+c = np.matmul(a, b)     # 矩阵乘法  要求 a 的列数 = b 的行数
+
+
+print(a)
+print(b)
+print(c)
+
+a = np.identity(3, 'int32')
+b = np.linalg.det(a)    # 求 行列式
+print(b)
+
+# 统计学
+a = np.array([[[4, 5, 6],
+               [1, 2, 3],
+               [7, 8, 9]],
+
+               [[1, 2, 3],
+                [4, 5, 6],
+                [1, 1, 1]]])
+
+print(np.min(a))
+print(np.min(a, axis= 0))  # axis= 0/1/2 分别是 消除层/行/列   即竖直方向/列/行元素取最小值, 变为 1 层/行/列
+
+print(np.sum(a))
+print(np.sum(a, axis= 2))
+
+a = np.array([[1, 2, 3],
+              [4, 5, 6]])
+b = a.reshape((6, 1))
+print(b)
+
+# 堆叠
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+v3 = np.vstack((v1, v2, v1))   # 竖向叠加
+print(v3)
+
+v1 = np.ones((2, 3), 'int32')
+v2 = np.zeros((2, 2), 'int32')
+v3 = np.hstack((v1, v2))       # 横向叠加
+print(v3)
+
+a = np.genfromtxt('word', delimiter= ',', dtype= 'int32')
+print(a)
+
+b = a > 1
+print(b)
+
+b = a[a > 1]
+print(b)
+
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+b = a[[0, 5, 8]]
+print(b)
